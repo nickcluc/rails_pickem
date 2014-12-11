dbaa128ee547e41a5a046ed370ca037cfa5404c2
Steven Watkins <steven.watkins78@gmail.com>
2014-12-11 13:46:05 -0500

    got individual games to populate based on week

---
+++ app/controllers/pick_controller.rb
@@ -0,0 +1,19 @@
+class PickController < ApplicationController
+
+  def api_connection(week_number)
+
+    api_key = ENV["NFL_API_KEY"]
+    uri = URI("https://profootballapi.com/schedule")
+    response = Net::HTTP.post_form(uri, { "api_key" => api_key,
+      "season"=>'REG',
+      "week" => "#{week_number}",
+      "year"=>2014,
+      })
+      JSON.parse(response.body)
+
+  end
+
+  def show
+    @games = api_connection(params["week"])
+  end
+end
